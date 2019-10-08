b83a9ac975598e1dd67171dcb3ddc6e11d25e4a4
SheepAntler <41525748+SheepAntler@users.noreply.github.com>
2019-10-08 16:18:28 -0500

    Added all files for my final project for Web Dev Fall 2019

---
+++ project05bWebsite/javaScriptFiles/featuredEggcorn.js
@@ -0,0 +1,53 @@
+/** This is the Master JavaScript file for the Lady Mondegreen and the Eggcorns Website
+   Author: Elspeth Stalter-Clouse
+   Date: 12/2/2018 
+*/
+
+function displayPhilippaAnswer() {
+
+	"use strict"; 
+
+	var answerParagraph;
+
+	answerParagraph = document.getElementById("philippa");
+
+	answerParagraph.innerHTML = "One night, before an evening concert at "
+		+ "a patron's country house, the serving staff "
+		+ "wasn't prepared for us to arrive as early as we did, "
+		+ "so they sent us round to the barn to rehearse. "
+		+ "When we got there, one of the mares was in labor, "
+		+ "so we had to go fetch one of the stable hands and then "
+		+ "rehearse with that whole...process going on in the background. "
+		+ "It made for quite an exciting rehearsal that none of us "
+		+ "soon forgot! Once we'd got inside, all our nerves were "
+		+ "completely stilled because the peace and quiet was such a welcome change!"
+		;
+}
+
+function displayLetticeAnswer() {
+
+	"use strict"; 
+
+	var answerParagraph;
+
+	answerParagraph = document.getElementById("philippa");
+
+	answerParagraph.innerHTML = "Oh, that's an easy one! Once, I had to practice "
+		+ "in a moving carriage on my way to London. "
+		+ "Philippa and I took it in turns travelling for lessons, "
+		+ "and one week she was too ill to go to London for hers, "
+		+ "so I had to take her place. Of course, because I hadn't "
+		+ "expected to have a lesson that day, I hadn't practiced as much as I  "
+		+ "would have liked, so in my 12-year-old mind the only solution was "
+		+ "to unpack my lute in the carriage, which was chock-full of muslin-clad "
+		+ "ladies on their way to town. Convincing the driver to let me take my lute "
+		+ "in the carriage was a real challenge, and then I had to deal with all "
+		+ "the ghastly looks for the entire 5 hours in the carriage. "
+		+ "I'm reasonably certain that, once I alighted, I heard a small cheer "
+		+ "erupt from within the carriage. I wasn't the world's best lutenist "
+		+ "to begin with at that time, and the jostling of the carriage certainly "
+		+ "did me no favors, so I really can't blame them in hindsight."
+		+ "At the time, however, it really did me in. I arrived at my lesson in tears!"
+		;
+}
+
